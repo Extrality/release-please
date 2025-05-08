871ef2566dec56f3097843502015f7fde648bb3c
Stan Tsouvallas <stan@stately.cloud>
2025-05-08 14:20:35 +1000

    cleanup

--- src/bin/release-please.ts
+++ src/bin/release-please.ts
@@ -309,7 +309,7 @@ function pullRequestStrategyOptions(yargs: yargs.Argv): yargs.Argv {
       type: 'string',
     })
     .option('extra-files', {
-      describe: 'extra files for the strategy to update',
+      describe: 'extra files for the strategy to consider',
       type: 'string',
       coerce(arg?: string) {
         if (arg) {
